17247897db00de0b0d39b1bf13170fcefa5f6ed1
CelesteRocco <ce.rocco@studenti.unina.it>
2026-05-26 11:01:53 +0200

    RisBufStaff
    RisBufStaff

--- Wellness/src/main/java/gui/StaffHome.java
+++ Wellness/src/main/java/gui/StaffHome.java
@@ -3,7 +3,37 @@ package gui;
 import controller.Controller;
 import model.utenti.Staff;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class StaffHome {
+    private JPanel mainPanel;
+    private JButton btnVerificaCertificati;
+    private JButton btnGestionePrenotazioni;
+    private JButton btnVisualizzaTurni;
+    private JButton btnLogout;
+    private JLabel lblTitolo;
+
+    public JPanel StaffPanel() {
+        return mainPanel;
+    }
+
     public StaffHome(Controller controller, Staff staff) {
+
+
+
+        btnVerificaCertificati.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(mainPanel, "Apertura modulo: Controllo Certificati Medici e Varco Accessi");
+            }
+        });
+
+        btnGestionePrenotazioni.addActionListener();
+
+
+
+
     }
 }

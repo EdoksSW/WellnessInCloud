89afbd7af4e840dcb4235fea060921948481e52c
CelesteRocco <ce.rocco@studenti.unina.it>
2026-05-27 09:47:31 +0200

    bug fix

--- Wellness/src/main/java/gui/AdminHome.java
+++ Wellness/src/main/java/gui/AdminHome.java
@@ -1,28 +1,26 @@
-package gui.AdminHome;
+package gui;
+
+import controller.Controller;
+import model.utenti.Admin;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import controller.Controller;
-import model.utenti.Admin;
 
 public class AdminHome {
-    Controller controller;
+
     private JPanel mainPanel;
-    private JButton btnVisualizzaStaff;
-    private JButton btnGestionePrezzario;
+    private JLabel lblTitolo;
     private JButton btnGestioneCorsi;
+    private JButton btnGestionePrezzario;
+    private JButton btnVisualizzaStaff;
     private JButton btnLogout;
-    private JLabel lblTitoli;
 
     public JPanel getAdminPanel() {
         return mainPanel;
     }
 
     public AdminHome(Controller controller, Admin admin) {
-            this.controller = controller;
-            Admin adminLoggato=admin;
-
 
         btnGestioneCorsi.addActionListener(new ActionListener() {
             @Override
@@ -34,27 +32,32 @@ public class AdminHome {
         btnGestionePrezzario.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(mainPanel, "Apertura modulo: Gestione Prezziario(Abbonamenti e Ingressi");
+                JOptionPane.showMessageDialog(mainPanel, "Apertura modulo: Gestione Prezzario (Abbonamenti e Ingressi)");
             }
         });
 
         btnVisualizzaStaff.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(mainPanel, "Apertura modulo: Gestone Staff e Assegnazione Turni");
+                JOptionPane.showMessageDialog(mainPanel, "Apertura modulo: Gestione Staff e Assegnazione Turni");
             }
         });
 
         btnLogout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int risposta = JOptionPane.showConfirmDialog(mainPanel, "sei sicuro di voler uscire?", "Logout", JOptionPane.YES_NO_OPTION);
+                int risposta = JOptionPane.showConfirmDialog(mainPanel,
+                        "Sei sicuro di voler uscire dal pannello Amministratore?",
+                        "Informativa di Logout",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE);
 
-                if (risposta==JOptionPane.YES_OPTION){
-                    JOptionPane.showMessageDialog(mainPanel, "Logout effettuato. Torno al Login.");
+                if (risposta == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog(mainPanel, "Logout effettuato con successo.");
                     System.exit(0);
                 }
             }
         });
     }
-    }
+}
+

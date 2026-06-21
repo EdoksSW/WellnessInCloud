0646282c4dc049e83947b6fbf7cfe86ff993f7b1
Gianluca Vassallucci <gianluca@velanet.info>
2026-06-21 16:08:30 +0200

    -homeclient.form ora è collegato -collegamento al db -bugfixing loginDb -aggiornato dump per aggiungere email nella tabella admin e con i 3 utenti

--- Wellness/src/main/java/controller/Controller.java
+++ Wellness/src/main/java/controller/Controller.java
@@ -1,4 +1,6 @@
 package controller;
+import dao.UtenteDAO;
+import implementazioniPostgresDAO.UtenteImplementazionePostgresDAO;
 import model.utenti.*;
 import model.enums.*;
 import model.commerce.*;
@@ -25,58 +27,11 @@ public class Controller
         prenotazioni = new ArrayList<>();
         iscrizioni = new ArrayList<>();
         ordini = new ArrayList<>();
-        initializeDatiMock();
-    }
-
-    private void initializeDatiMock() {
-
-        utenti.add(new Admin(
-                "VSSGNC06E25F839A",
-                "Gianluca",
-                "Vassallucci",
-                "admin@wellness.it",
-                "081000000",
-                "admin123",
-                LocalDate.of(2006, 5, 25)
-        ));
-
-        // 2. Inserimento diretto dello STAFF
-        utenti.add(new Staff(
-                "RSSMRC95C12F839B",
-                "Marco",
-                "Rossi",
-                "staff@wellness.it",
-                "081111111",
-                "staff123",
-                LocalDate.of(1995, 3, 12),
-                "Laurea Magistrale",
-                "IT12X0000000000000000",
-                RuoloStaff.RECEPTIONIST
-        ));
-
-        // 3. Inserimento diretto del CLIENTE (con civico come int 21)
-        utenti.add(new Cliente(
-                "VRDNTN00R05F839C",
-                "Antonio",
-                "Verdi",
-                "cliente@wellness.it",
-                "081222222",
-                "cliente123",
-                LocalDate.of(2000, 10, 5),
-                "Via Claudio",
-                21,
-                "80125",
-                StatoAccount.ATTIVO
-        ));
     }
 
     public Utente login(String email, String password) {
-        for (Utente u : utenti) {
-            if (u.getEmail().equalsIgnoreCase(email) && u.getPassword().equals(password)) {
-                return u;
-            }
-        }
-        return null;
+        UtenteDAO utenteDAO = new UtenteImplementazionePostgresDAO();
+        return utenteDAO.loginDB(email, password);
     }
 
     public String messaggioAccessoNegato(Utente u) {

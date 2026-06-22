ee91886a90f60aef8a856064a79ccb7311191ee6
EdoksSW <e.peiko@studenti.unina.it>
2026-06-22 19:25:15 +0200

    Update ClienteImplementazionePostgresDAO.java
    Appena completata la funzione trovaPerCodiceFiscale un Cliente

--- Wellness/src/main/java/implementazioniPostgresDAO/ClienteImplementazionePostgresDAO.java
+++ Wellness/src/main/java/implementazioniPostgresDAO/ClienteImplementazionePostgresDAO.java
@@ -3,6 +3,7 @@ package implementazioniPostgresDAO;
 import dao.ClienteDAO;
 import database.ConnessioneDatabase;
 import model.commerce.Carrello;
+import model.enums.StatoAccount;
 import model.logistica.Prenotazione;
 import model.utenti.Cliente;
 
@@ -40,11 +41,29 @@ public class ClienteImplementazionePostgresDAO implements ClienteDAO
             preparedStatement.setString(1,codiceFiscale);
             try(ResultSet resultSet=preparedStatement.executeQuery())
             {
+                 String email=resultSet.getString("email");
+                 String password=resultSet.getString("password");
+                 String nome=resultSet.getString("nome");
+                 String cognome=resultSet.getString("cognome");
+                 String telefono=resultSet.getString("telefono");
+                 String via=resultSet.getString("via");
+                 int numCivico=resultSet.getInt("civico");
+                 String cap=resultSet.getString("cap");
 
+                 String stato_acc=resultSet.getString("stato_account");
+                 StatoAccount statoAccount=StatoAccount.valueOf(stato_acc.trim().toUpperCase());
+
+                 LocalDate dataNascita=null;
+                 if(resultSet.getDate("datanascita")!=null)
+                 {
+                     dataNascita=resultSet.getDate("datanascita").toLocalDate();
+                 }
+                 return new Cliente(codiceFiscale,nome, cognome, email, telefono, password, dataNascita, via, numCivico, cap, statoAccount);
             }
-        }catch(SQLException e)
-        {
-
+        }catch(SQLException e) {
+            System.out.println("Errore SQL in trovaPerCodiceFiscale: " + e.getMessage());
+            e.printStackTrace();
+            return null;
         }
     }
 

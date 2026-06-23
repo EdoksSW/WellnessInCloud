6360273dfc49cb2a7627a1e0b557f85e8f1c3087
EdoksSW <e.peiko@studenti.unina.it>
2026-06-23 18:16:27 +0200

    Non terminato
    aggiornamento

--- Wellness/src/main/java/model/commerce/Carrello.java
+++ Wellness/src/main/java/model/commerce/Carrello.java
@@ -6,8 +6,39 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class Carrello {
-    private int id;
+    private int id_carrello;
     private Cliente cliente;
     private ArrayList<Prodotto>prodotto;
     private BigDecimal totale;
+
+    public Carrello(Cliente cliente, int id_carrello, BigDecimal totale) {
+        this.cliente = cliente;
+        this.id_carrello = id_carrello;
+        this.totale = totale;
+    }
+
+    public BigDecimal getTotale() {
+        return totale;
+    }
+
+    public void setTotale(BigDecimal totale) {
+        this.totale = totale;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getId_carrello() {
+        return id_carrello;
+    }
+
+    public void setId_carrello(int id_carrello) {
+        this.id_carrello = id_carrello;
+    }
 }
+
--- Wellness/src/main/java/model/commerce/Prodotto.java
+++ Wellness/src/main/java/model/commerce/Prodotto.java
@@ -3,7 +3,57 @@ package model.commerce;
 import java.math.BigDecimal;
 
 public class Prodotto {
+    int id_prodotto;
     private String nome;
     private BigDecimal prezzo;
     private int giacenza;
+    String categoria;
+
+    public Prodotto(int id_prodotto, String nome, BigDecimal prezzo, int giacenza, String categoria) {
+        this.id_prodotto = id_prodotto;
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.giacenza = giacenza;
+        this.categoria = categoria;
+    }
+
+    public int getId_prodotto() {
+        return id_prodotto;
+    }
+
+    public void setId_prodotto(int id_prodotto) {
+        this.id_prodotto = id_prodotto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public int getGiacenza() {
+        return giacenza;
+    }
+
+    public void setGiacenza(int giacenza) {
+        this.giacenza = giacenza;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }

2a6bd32cfad432531292dc21d187d4d9c57e9126
EdoksSW <e.peiko@studenti.unina.it>
2026-07-01 13:56:26 +0200

    Implemtazioni
    Creata Classe OrdineDettaglio
    Implementato in ClienteImplementazionePostgresDAO le seguenti funzioni:
    ottiniDettagliOrdine
    ottieniStoricoOrdini

--- Wellness/src/main/java/model/commerce/Pagamento.java
+++ Wellness/src/main/java/model/commerce/Pagamento.java
@@ -2,8 +2,44 @@ package model.commerce;
 
 import java.math.BigDecimal;
 
-public abstract class Pagamento {
+public class Pagamento {
     private BigDecimal importo;
     private String metodo;
-    private int id;
+    private int id_pagamento;
+
+    
+    public Pagamento(){}
+    
+    public Pagamento(BigDecimal importo, String metodo, int id_pagamento) {
+        this.importo = importo;
+        this.metodo = metodo;
+        this.id_pagamento = id_pagamento;
+    }
+
+    public BigDecimal getImporto() {
+        return importo;
+    }
+
+    public void setImporto(BigDecimal importo) {
+        this.importo = importo;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+
+    public int getId_pagamento() {
+        return id_pagamento;
+    }
+
+    public void setId_pagamento(int id_pagamento) {
+        this.id_pagamento = id_pagamento;
+    }
+
+    public void set() {
+    }
 }

b997ed3ad56436ef88d932933da5ecafc0748027
CelesteRocco <ce.rocco@studenti.unina.it>
2026-07-01 13:19:31 +0200

    Implementate GUI e logica Staff per clienti e certificati, con comunicazione a SQL

--- Wellness/src/main/java/dao/StaffDAO.java
+++ Wellness/src/main/java/dao/StaffDAO.java
@@ -8,4 +8,13 @@ public interface StaffDAO {
     ArrayList<Staff> getAllStaff();
 
     ArrayList<Staff> getIstruttori();
+
+    ArrayList<model.utenti.Cliente> getAllClientiDaStaff();
+    boolean aggiungiClienteDaStaff(model.utenti.Cliente cliente);
+    boolean modificaClienteDaStaff(model.utenti.Cliente cliente);
+    boolean rimuoviClienteDaStaff(String codiceFiscale);
+
+    java.time.LocalDate getCertificatoDaStaff(String codiceFiscale);
+    boolean aggiornaCertificatoDaStaff(String codiceFiscale, java.time.LocalDate nuovaScadenza);
+
 }
